3bb4cb03137f52470763f549458d53a452437cef
ElRaptorus <christian.werner@5minds.de>
2018-10-09 13:23:29 +0200

    :sparkles: Add auth check to internal accessor

--- src/accessors/internal_accessor.ts
+++ src/accessors/internal_accessor.ts
@@ -1,3 +1,4 @@
+import {UnauthorizedError} from '@essential-projects/errors_ts';
 import {IIdentity} from '@essential-projects/iam_contracts';
 
 import {
@@ -19,14 +20,23 @@ export class ExternalTaskApiInternalAccessor implements IExternalTaskApi {
                                          maxTasks: number,
                                          longPollingTimeout: number,
                                          lockDuration: number): Promise<Array<ExternalTask>> {
+
+    this._ensureIsAuthorized(identity);
+
     return this._externalApiService.fetchAndLockExternalTasks(identity, workerId, topicName, maxTasks, longPollingTimeout, lockDuration);
   }
 
   public async extendLock(identity: IIdentity, workerId: string, externalTaskId: string, additionalDuration: number): Promise<void> {
+
+    this._ensureIsAuthorized(identity);
+
     return this._externalApiService.extendLock(identity, workerId, externalTaskId, additionalDuration);
   }
 
   public async handleBpmnError(identity: IIdentity, workerId: string, externalTaskId: string, errorCode: string): Promise<void> {
+
+    this._ensureIsAuthorized(identity);
+
     return this._externalApiService.handleBpmnError(identity, workerId, externalTaskId, errorCode);
   }
 
@@ -35,10 +45,26 @@ export class ExternalTaskApiInternalAccessor implements IExternalTaskApi {
                                   externalTaskId: string,
                                   errorMessage: string,
                                   errorDetails: string): Promise<void> {
+
+    this._ensureIsAuthorized(identity);
+
     return this._externalApiService.handleServiceError(identity, workerId, externalTaskId, errorMessage, errorDetails);
   }
 
   public async finishExternalTask(identity: IIdentity, workerId: string, externalTaskId: string, payload: any): Promise<void> {
+
+    this._ensureIsAuthorized(identity);
+
     return this._externalApiService.finishExternalTask(identity, workerId, externalTaskId, payload);
+  }
+
+  private _ensureIsAuthorized(identity: IIdentity): void {
+
+    // Note: When using an external accessor, this check is performed by the ConsumerApiHttp module.
+    // Since that component is bypassed by the internal accessor, we need to perform this check here.
+    const authTokenNotProvided: boolean = !identity || typeof identity.token !== 'string';
+    if (authTokenNotProvided) {
+      throw new UnauthorizedError('No auth token provided!');
+    }
   }
 }
