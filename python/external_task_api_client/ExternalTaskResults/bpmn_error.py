631aa49743d6c8b525fbfea532327f86df51ef1a
Christoph <christoph.gnip@gmail.com>
2018-11-09 17:43:59 +0100

    :recycle: Adjust the Code Layout

--- python/external_task_api_client/ExternalTaskResults/bpmn_error.py
+++ python/external_task_api_client/ExternalTaskResults/bpmn_error.py
@@ -5,4 +5,11 @@ class ExternalTaskBpmnError:
         self.__error_details = error_details
 
     async def send_to_external_task_api(self, external_task_api, identity, worker_id):
-        await external_task_api.handle_bpmn_error(identity, worker_id, self.__external_task_id, self.__error_message, self.__error_details)
+        await external_task_api
+            .handle_bpmn_error(
+                identity,
+                worker_id,
+                self.__external_task_id,
+                self.__error_message,
+                self.__error_details
+            )
--- python/external_task_api_client/ExternalTaskResults/finished.py
+++ python/external_task_api_client/ExternalTaskResults/finished.py
@@ -4,4 +4,9 @@ class ExternalTaskFinished:
         self.__result = result
 
     async def send_to_external_task_api(self, external_task_api, identity, worker_id):
-        await external_task_api.finish_external_task(identity, worker_id, self.__external_task_id, self.__result)
+        await external_task_api.finish_external_task(
+                identity,
+                worker_id,
+                self.__external_task_id,
+                self.__result
+            )
